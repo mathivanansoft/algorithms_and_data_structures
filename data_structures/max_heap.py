01c5f93815137e942682c84eba2dd41859e57f9c
mathivanansoft <mathivanansoft@gmail.com>
2020-02-11 20:06:04 +0530

    heap modified

--- data_structures/max_heap.py
+++ data_structures/max_heap.py
@@ -1,76 +1,89 @@
-def max_heapify(arr, index, length):
-    left = 2 * index + 1
-    right = 2 * index + 2
+class MaxHeap(object):
+    def __init__(self, arr):
+        self.arr = arr
+
+    def build_heap(self):
+        children = len(self.arr) // 2
+        for index in range(children, -1, -1):
+            self.heapify(index)
     
-    if left < length and arr[index] < arr[left]:
-        largest = left
-    else:
-        largest = index
-    
-    if right < length and arr[largest] < arr[right]:
-        largest = right
-    
-    if largest != index:
-        temp = arr[largest]
-        arr[largest] = arr[index]
-        arr[index] = temp
-        max_heapify(arr, largest, length)
-
-def build_max_heap(arr):
-    children = len(arr) // 2
-    for index in range(children, -1, -1):
-        max_heapify(arr, index, len(arr))
-
-def extract_max(arr):
-    if not len(arr):
-        print("HEAP is empty")
-    else:
-        maximum = arr[0]
-        arr[0] = arr[-1]
-        arr.pop()
-        max_heapify(arr, 0, len(arr))
-        return maximum
-
-def insert(arr, elmnt):
-    arr.append(elmnt)
-    l = len(arr) - 1
-    parent = l // 2
-    if (l & 1) != 1:
-        parent -= 1
-
-    while parent >= 0:
-        if arr[l] > arr[parent]:
-            temp = arr[parent]
-            arr[parent] = arr[l]
-            arr[l] = temp
-            l = parent
-            if (parent & 1) != 1:
-                parent = parent // 2
-                parent -= 1
-            else:
-                parent = parent // 2
+    def heapify(self, index):
+        length = len(self.arr)
+        left = 2 * index + 1
+        right = 2 * index + 2
+        
+        if left < length and self.arr[index] < self.arr[left]:
+            largest = left
         else:
-            break
+            largest = index
+
+        if right < length and self.arr[largest] < self.arr[right]:
+            largest = right
+        
+        if largest != index:
+            temp = self.arr[largest]
+            arr[largest] = self.arr[index]
+            self.arr[index] = temp
+            self.heapify(largest)
+
+
+    def extract_max(self):
+        if not len(self.arr):
+            raise Exception("HEAP is empty")
+        else:
+            maximum = self.arr[0]
+            self.arr[0] = self.arr[-1]
+            self.arr.pop()
+            self.heapify(0)
+            return maximum
+
+    def insert(elmnt):
+        self.arr.append(elmnt)
+        l = len(self.arr) - 1
+        parent = l // 2
+        if (l & 1) != 1:
+            parent -= 1
+
+        while parent >= 0:
+            if self.arr[l] > self.arr[parent]:
+                temp = self.arr[parent]
+                self.arr[parent] = self.arr[l]
+                self.arr[l] = temp
+                l = parent
+                if (parent & 1) != 1:
+                    parent = parent // 2
+                    parent -= 1
+                else:
+                    parent = parent // 2
+            else:
+                break
+
+    def __len__(self):
+        return len(self.arr)
+
+    def __repr__(self):
+        return str(self.arr)
 
 if __name__ == "__main__":
     arr = [1, 2, 3, 4, 5]
-    build_max_heap(arr)
+    obj = MaxHeap(arr)
+    obj.build_heap()
     # extract max is similar to delete max.
-    print("AFTER MAX HEAP: ", arr)
-    maxi = extract_max(arr)
+    print("AFTER MAX HEAP: ", obj)
+    maxi = obj.extract_max()
     print("EXTRACTED element", maxi)
-    print("AFTER EXTRACTION", arr)
-    maxi = extract_max(arr)
+    print("AFTER EXTRACTION", obj)
+    maxi = obj.extract_max()
     print("EXTRACTED element", maxi)
-    print("AFTER EXTRACTION", arr)
-    maxi = extract_max(arr)
+    print("AFTER EXTRACTION", obj)
+    maxi = obj.extract_max()
     print("EXTRACTED element", maxi)
-    print("AFTER EXTRACTION", arr)
-    maxi = extract_max(arr)
+    print("AFTER EXTRACTION", obj)
+    maxi = obj.extract_max()
     print("EXTRACTED element", maxi)
-    print("AFTER EXTRACTION", arr)
-    maxi = extract_max(arr)
+    print("AFTER EXTRACTION", obj)
+    maxi = obj.extract_max()
     print("EXTRACTED element", maxi)
-    print("AFTER EXTRACTION", arr)
+    print("AFTER EXTRACTION", obj)
 
     
--- data_structures/min_heap.py
+++ data_structures/min_heap.py
@@ -1,78 +1,84 @@
+class MinHeap(object):
+    def __init__(self, arr=[]):
+        self.arr = arr
 
-def build_min_heap(arr):
-    l = len(arr)
-    end = l // 2 - 1
-    for index in range(end, -1, -1):
-        min_heapify(arr, index)
+    def build_heap(self):
+        l = len(self.arr)
+        end = l // 2 - 1
+        for index in range(end, -1, -1):
+            self.heapify(index)
 
-def min_heapify(arr, index):
-    left = 2 * index + 1
-    right = 2 * index + 2
+    def heapify(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
 
-    mininum = index
-    if left < len(arr) and arr[index] > arr[left]:
-        mininum = left
-    if right < len(arr) and arr[mininum] > arr[right]:
-        mininum = right
-    if mininum != index:
-        temp = arr[mininum]
-        arr[mininum] = arr[index]
-        arr[index] = temp
-        min_heapify(arr, mininum)
+        mininum = index
+        if left < len(self.arr) and self.arr[index] > self.arr[left]:
+            mininum = left
+        if right < len(self.arr) and self.arr[mininum] > self.arr[right]:
+            mininum = right
+        if mininum != index:
+            temp = self.arr[mininum]
+            self.arr[mininum] = self.arr[index]
+            self.arr[index] = temp
+            self.heapify(mininum)
 
-def extract_min(arr):
-    if not len(arr):
-        print("HEAP is empty")
-    else:
-        minimum = arr[0]
-        arr[0] = arr[-1]
-        arr.pop()
-        min_heapify(arr, 0)
-        return minimum
-
-def insert(arr, elmnt):
-    arr.append(elmnt)
-    l = len(arr) - 1
-    parent = l // 2
-    if (l & 1) != 1:
-        parent -= 1
-
-    while parent >= 0:
-        if arr[parent] > arr[l]:
-            temp = arr[parent]
-            arr[parent] = arr[l]
-            arr[l] = temp
-            l = parent
-            if (parent & 1) != 1:
-                parent = parent // 2
-                parent -= 1
-            else:
-                parent = parent // 2
+    def extract_min(self):
+        if not len(self.arr):
+            raise Exception("HEAP is empty")
         else:
-            break
+            minimum = self.arr[0]
+            self.arr[0] = self.arr[-1]
+            self.arr.pop()
+            self.heapify(0)
+            return minimum
 
+    def insert(self, elmnt):
+        self.arr.append(elmnt)
+        l = len(self.arr) - 1
+        parent = l // 2
+        if (l & 1) != 1:
+            parent -= 1
+
+        while parent >= 0:
+            if self.arr[parent] > self.arr[l]:
+                temp = self.arr[parent]
+                self.arr[parent] = self.arr[l]
+                self.arr[l] = temp
+                l = parent
+                if (parent & 1) != 1:
+                    parent = parent // 2
+                    parent -= 1
+                else:
+                    parent = parent // 2
+            else:
+                break
+
+    def __len__(self):
+        return len(self.arr)
+
+    def __repr__(self):
+        return str(self.arr)
 
 if __name__ == "__main__":
     arr = [5,4,3,2,1]
     print("MIN HEAP", arr)
-    build_min_heap(arr)
+    obj = MinHeap(arr)
+    obj.build_heap()
     # extract min is similar to delete min.
-    print("AFTER MIN HEAP: ", arr)
-    minimum = extract_min(arr)
+    print("AFTER MIN HEAP: ", obj)
+    minimum = obj.extract_min()
     print("EXTRACTED element", minimum)
-    print("AFTER EXTRACTION", arr)
-    minimum = extract_min(arr)
+    print("AFTER EXTRACTION", obj)
+    minimum = obj.extract_min()
     print("EXTRACTED element", minimum)
-    print("AFTER EXTRACTION", arr)
-    minimum = extract_min(arr)
+    print("AFTER EXTRACTION", obj)
+    minimum = obj.extract_min()
     print("EXTRACTED element", minimum)
-    print("AFTER EXTRACTION", arr)
-    minimum = extract_min(arr)
+    print("AFTER EXTRACTION", obj)
+    minimum = obj.extract_min()
     print("EXTRACTED element", minimum)
-    print("AFTER EXTRACTION", arr)
-    minimum = extract_min(arr)
+    print("AFTER EXTRACTION", obj)
+    minimum = obj.extract_min()
     print("EXTRACTED element", minimum)
-    print("AFTER EXTRACTION", arr)
-    
-    
-    
+    print("AFTER EXTRACTION", obj)
