fcb626561abf5c533a0ac0799a986a995a1d7284
mathivanansoft <mathivanansoft@gmail.com>
2020-03-05 23:25:35 +0530

    height calculated

--- data_structures/binary_tree.py
+++ data_structures/binary_tree.py
@@ -66,3 +66,8 @@ class BinaryTree(object):
         left = self.__size(node.left)
         right = self.__size(node.right)
         return 1 + left + right
+
+    def height(self, node):
+        if node is None:
+            return 0
+        return 1 + max(self.height(node.left), self.height(node.right))
